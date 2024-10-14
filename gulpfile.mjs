9df6b9eeb67dd8619d7b90392d829af2d37de890
lilliputten <lilliputten@gmail.com>
2024-10-14 18:28:43 +0300

    Final version of gulp code.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,8 +7,6 @@ import zip from 'gulp-zip';
 
 import map from 'map-stream';
 
-import stream from 'stream';
-
 import dayjsUtc from 'dayjs/plugin/utc.js';
 import dayjsTimezone from 'dayjs-timezone-iana-plugin'; // @see https://day.js.org/docs/en/plugin/timezone
 import dayjs from 'dayjs'; // @see https://day.js.org/docs/en/display/format
@@ -23,29 +21,11 @@ const timeZone = config.timeZone || '';
 
 // NOTE: Date formats for 'dayjs', @see https://day.js.org/docs/en/display/format
 const tagFormat = 'YYMMDD-HHmm';
-const timeTzFormat = 'YYYY.MM.DD HH:mm ZZ';
+// const timeTzFormat = 'YYYY.MM.DD HH:mm ZZ';
 
 const now = new Date();
 const buildTag = formatDate(now, timeZone, tagFormat);
-const buildTzTime = formatDate(now, timeZone, timeTzFormat);
-
-// var folderStream = new stream.Writable();
-// folderStream._write = function (chunk, encoding, done) {
-//   console.log('EEE', chunk);
-//   debugger;
-//   done();
-// };
-
-const folderStream = new stream.Writable({
-  write: function (chunk, encoding, next) {
-    console.log(chunk.toString());
-    next();
-  },
-});
-
-// console.log('buildTag', buildTag);
-// debugger;
-// process.exit(99);
+// const buildTzTime = formatDate(now, timeZone, timeTzFormat);
 
 const allFolders = [];
 
@@ -64,84 +44,42 @@ function formatDate(date, timeZone, fmt) {
   return fmtDate;
 }
 
-/** @param {string} folder */
-function finishTargetFolder(folder) {
-  const targetFolder = 'build/' + folder + '/';
-  return gulp
-    .src(targetFolder + '*')
-    .pipe(zip('archive.zip'))
-    .pipe(gulp.dest('arc'));
-  // .on('end', cb),
-  // return gulp.series(
-  //   // prettier-ignore
-  //   // (cb) =>
-  //   //   gulp
-  //   //     .src(['src/*'])
-  //   //     .pipe(gulp.dest(targetFolder))
-  //   //     .on('end', cb),
-  //   (cb) =>
-  //     gulp
-  //       .src(targetFolder + '*')
-  //       .pipe(zip('archive.zip'))
-  //       .pipe(gulp.dest('arc'))
-  //       .on('end', cb),
-  // );
-}
-
 function processSounds() {
-  // console.log('YYY', _done);
-  return (
-    gulp
-      .src(['sounds/**/*.mp3'])
-      // .on(
-      //   'data',
-      //   [>* @param {import('vinyl')} file <]
-      //   (file) => {
-      //     const { basename } = file;
-      //     const name = basename.replace(/\..*$/, '');
-      //     console.log('ZZZ', name);
-      //     return (
-      //       gulp
-      //         .src(['src/*'] [> , { base: './' } <])
-      //         // prettier-ignore
-      //         .pipe(gulp.dest('build/' + name + '/'))
-      //     );
-      //   },
-      // )
-      .pipe(
-        rename((path) => {
-          const { basename } = path;
-          // console.log('File', basename);
-          allFolders.push(basename);
-          path.dirname += '/' + basename;
-          path.basename = 'file';
-        }),
-      )
-      .pipe(gulp.dest('folders/'))
-      .pipe(
-        // @ts-ignore: WritableStream
-        map((/** @type {import('vinyl')} */ file, callback) => {
-          const { relative, base, path: pathName, basename } = file;
-          const folder = relative.replace(/[\\/].*/, '');
-          const targetFolder = 'folders/' + folder + '/';
-          const arcName = folder + '-' + buildTag + '.zip'
-          // console.log('MAP', folder);
-          gulp.series([
-            (cb) =>
-              gulp
-                // prettier-ignore
-                .src(['src/*'])
-                .pipe(gulp.dest(targetFolder))
-                .on('end', cb),
-            (cb) =>
-              gulp
-                .src(targetFolder + '*')
-                .pipe(zip(folder + '.zip'))
-                .pipe(gulp.dest('archives'))
-                .on('end', cb),
-          ])(() => callback(null, file));
-        }),
-      )
-  );
+  return gulp
+    .src(['sounds/**/*.mp3'], { encoding: false })
+    .pipe(
+      rename((path) => {
+        const { basename } = path;
+        // console.log('File', basename);
+        allFolders.push(basename);
+        path.dirname += '/' + basename;
+        path.basename = 'file';
+      }),
+    )
+    .pipe(gulp.dest('folders/'))
+    .pipe(
+      // @ts-ignore: WritableStream
+      map((/** @type {import('vinyl')} */ file, callback) => {
+        const { relative } = file;
+        const folder = relative.replace(/[\\/].*/, '');
+        const targetFolder = 'folders/' + folder + '/';
+        const arcName = folder + '-' + buildTag + '.zip';
+        // console.log('MAP', folder);
+        gulp.series([
+          (cb) =>
+            gulp
+              // prettier-ignore
+              .src(['src/**/*', '!**/*.mp3'], { encoding: false })
+              .pipe(gulp.dest(targetFolder))
+              .on('end', cb),
+          (cb) =>
+            gulp
+              .src(targetFolder + '**/*', { encoding: false })
+              .pipe(zip(arcName))
+              .pipe(gulp.dest('archives'))
+              .on('end', cb),
+        ])(() => callback(null, file));
+      }),
+    );
 }
 gulp.task('processSounds', processSounds);
